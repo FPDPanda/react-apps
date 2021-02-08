c27f8dc98e3a29b3f71b8ed09913bf55937b1917
FPDPanda <jeanjacques7.jj@gmail.com>
2021-02-07 23:12:40 -0300

    [Creating] Main component paths

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import React from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom'
+
+// Main Components
 
 function App() {
   return (
+    <Router basename="/">
+      <Route path="/home" component={Home} />
+    </Router>
+  )
 }
 
 export default App
