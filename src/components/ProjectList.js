6a341caa27abeeeba991ec0d94e1bbead5e0bb4b
FPDPanda <jeanjacques7.jj@gmail.com>
2021-02-08 20:40:25 -0300

    [Updated] Component - Project List
    Better UI/UX with vertical scrolling insted of horizontal

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -18,24 +18,25 @@ function ProjectList() {
             key: 1,
             image: img1,
             title: "Recipe Search",
-            description: "Search for recipies using an API",
-            link: "/project01"            
+            description: "Basic project made with React where you can search for recipes using an API powered by EDAMAN.",
+            link: "/project01",
         }
-
         ])
     }, []);
 
     return (
-
         <ul id="project-list">
             {projects.map(project =>
                 <li className="project" key={project.key}>
                     <img src={project.image} alt="screenshot"></img>
-                    <h2>{project.title}</h2>
-                    <p>{project.description}</p>
-                    <Router>
-                        <Link to={project.link}>Link to project</Link>
-                    </Router>
+                    <article className="project-article">
+                        <h2 className="project-title">{project.title}</h2>
+                        <p className="project-description">{project.description}</p>
+
+                        <Router>
+                            <Link className="project-link" to={project.link}>Link to project</Link>
+                        </Router>
+                    </article>
                 </li>
             )}
             
@@ -44,7 +45,6 @@ function ProjectList() {
             //Annoying CSS bug with overflow-x more details on the link
             //https://alexandergottlieb.com/2018/02/22/overflow-scroll-and-the-right-padding-problem-a-css-only-solution
             }
-            <li id="last"></li>
         </ul>
     )
 }
