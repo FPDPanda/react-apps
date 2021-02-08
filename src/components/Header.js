9acb3e0f70c412a8d99e3a48f5927fe9643376b5
FPDPanda <jeanjacques7.jj@gmail.com>
2021-02-07 23:14:01 -0300

    [Creating] Component - Header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// CSS
+import './Header.css'
+
+function Header() {
+    return (
+        <header>
+            <h2 id="welcome">Welcome to my React projects</h2>
+            <h2 id="credits">Jean Jacques</h2>
+        </header>
+    )
+}
+
+export default Header
--- src/main components/Home.js
+++ src/main components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 // Components
+import Header from '../components/Header'
 
 function Home() {
     return (
         <>
+            <Header />
         </>
     )
 }
