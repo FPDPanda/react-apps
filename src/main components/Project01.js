5b6483f0c4c1356e96089a2634e24a71e53bc22c
FPDPanda <jeanjacques7.jj@gmail.com>
2021-02-08 19:23:16 -0300

    [Adding] Recipes API

--- src/main components/Project01.js
+++ src/main components/Project01.js
@@ -9,11 +9,33 @@ import './Project01.css';
 
 function Project01() {
 
+    const APP_ID = "25fbb60d";
+    const APP_KEY = "875753a058beb4a482ef8758f05b4399";
+
+    const [recipes, setRecipes] = useState([]);
     const [search, setSearch] = useState('');
+    const [query, setQuery] = useState('chicken');
+
+    useEffect(() => {
+        getRecipes();
+    }, [query]);
+
+    async function getRecipes() {
+        const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+        const data = await response.json();
+
+        setRecipes(data.hits);
+    };
+
     function updateSearch(e) {
         setSearch(e.target.value);
     }
 
+    function getSearch(e) {
+        e.preventDefault();
+        setQuery(search);
+        setSearch('');
+    }
 
     return (
         <div className ="project01">
